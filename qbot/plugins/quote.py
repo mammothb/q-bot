f35e680ca3f9148f18b42f003a62c3f58e23781d
mammothb <mammothb+git@gmail.com>
2018-09-17 14:14:20 +0800

    updated quote plugin to accept message content as well

--- qbot/plugins/quote.py
+++ qbot/plugins/quote.py
@@ -2,9 +2,13 @@
 from datetime import datetime, timedelta
 import logging
 
+from discord.errors import NoMoreItems
+from symspellpy.symspellpy import SymSpell
+
 from qbot.const import PREFIX, TZ_OFFSET
 from qbot.decorators import command
 from qbot.plugin import Plugin
+from qbot.utility import try_parse_int64
 
 LOG = logging.getLogger("discord")
 
@@ -13,18 +17,41 @@ class Quote(Plugin):
              description="Format quote message as a code block",
              usage=PREFIX + "quote msg_id")
     async def quote(self, message, args):
-        msg_id = args[0]
-        try:
-            msg = await self.client.get_message(message.channel.id, msg_id)
+        msg = None
+        if try_parse_int64(args[0]) is not None:
+            msg_id = args[0]
+            try:
+                msg = await self.client.get_message(message.channel.id, msg_id)
+            except Exception as exception:  # pylint: disable=W0703
+                LOG.exception(exception)
+        else:
+            input_term = args[0]
+            sym_spell = SymSpell()
+            for term in input_term.split(" "):
+                sym_spell.create_dictionary_entry(term, 1)
+            target = sym_spell.lookup_compound(input_term, 2)[0].term
+            iterator = message.channel.history(limit=100)
+            for __ in range(100):
+                try:
+                    msg = await iterator.next()
+                    suggestion = sym_spell.lookup_compound(msg.content, 2)[0]
+                    if suggestion.term == target:
+                        msg = await self.client.get_message(
+                            message.channel.id, msg.id)
+                        break
+                except NoMoreItems:
+                    msg = None
+        if msg is not None:
             display_name = message.guild.get_member(
                 int(msg["author"]["id"])).display_name
             time_str = (datetime.strptime(msg["timestamp"].split(".")[0],
                                           "%Y-%m-%dT%H:%M:%S") +
-                        timedelta(hours=TZ_OFFSET)).strftime("%Y-%m-%d %I:%M %p")
+                        timedelta(hours=TZ_OFFSET)).strftime(
+                            "%Y-%m-%d %I:%M %p")
             quote_msg = "```{} - {} UTC+{}\n{}```".format(
                 display_name, time_str, TZ_OFFSET, msg["content"])
-        except Exception as exception:  # pylint: disable=W0703
-            LOG.exception(exception)
+        else:
             quote_msg = "Message not found!"
+
         await self.client.send_message(message.channel.id, quote_msg)
         await message.delete()
--- qbot/utility.py
+++ qbot/utility.py
@@ -13,3 +13,10 @@ def replace_multiple(replacement, string):
                        for key, val in replacement.items())
     pattern = re.compile("|".join(replacement.keys()))
     return pattern.sub(lambda m: replacement[re.escape(m.group(0))], string)
+
+def try_parse_int64(string):
+    try:
+        ret = int(string)
+    except ValueError:
+        return None
+    return None if ret < -2 ** 64 or ret >= 2 ** 64 else ret
